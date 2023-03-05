17dc5eb1d2e6b941bc168095da0aae5e204f08ec
Mohamad Al Khatib <mohamad@juno.tech>
2023-03-05 13:22:22 +0100

    Remove dead code

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,10 @@
 import express, { Application } from 'express';
-import Routes from '../Routes/index';
-import {JWT_SECRET} from "../config/index"
-import authenticate from '../middleware/authenticate';
-
-
+import Routes from "../routes/index";
+import authenticate from "../middleware/authenticate";
 
 const app: Application = express();
 
 app.use(express.json());
-// app.use(
-//     jwt({
-//         secret: JWT_SECRET,
-//         algorithms: ['HS256'],
-//     }).unless({ path: ['/login'] })
-// );
 
 app.use(authenticate);
 
